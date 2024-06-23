82de3febe6fa835515be667d099d9c96c72552ea
Ilya Chudakov <icorani@ya.ru>
2024-06-23 22:47:55 +0300

    Hello John Wilson

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'John Wilson'
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Welcome to Bizza project!</h1>
+      <h1>Welcome to Bizza project, {name}!</h1>
       </header>
     </div>
   );
